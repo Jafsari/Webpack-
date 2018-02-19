53ac26ee5249249154f8e8d2380ead886121b02b
Jafsari <justinafsari@yahoo.com>
2018-02-19 13:12:12 -0800

    added codesplitting for assets

--- sampleProject/webpack.config.js
+++ sampleProject/webpack.config.js
@@ -1,11 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+'react','lodash','redux','react-redux','react-dom','faker','react-input-range','redux-form',
+'redux-thunk'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: { // There will be multiple entry points now
+      bundle:'./src/index.js',//For the bundle.js file, start here
+      vendor: VENDOR_LIBS // Seperate bundle/file for all the dependencies
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module:{
   	rules:[{
@@ -18,5 +26,11 @@ module.exports = {
         test:/\.css$/
       }
     ]
-  }
+  },
+  plugins:[ 
+    new webpack.optimize.CommonsChunkPlugin({ // Look at the total sum of all the project files
+      name:'vendor' // If any of the dependencies in vendor and bundle.js overlap,
+      // add them only to the vendor file
+    }) 
+  ]
 };
