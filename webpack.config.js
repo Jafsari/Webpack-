d2f89719ca71a3059808622d1db2302a821b2bf0
Jafsari <justinafsari@yahoo.com>
2018-02-18 18:54:23 -0800

    adding image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const config = {
 entry:'./src/index.js', //entry point when the app boots up
@@ -7,8 +8,9 @@ output:{
     //__dirname is a reference to the current working directory.
     // Whenever webpack runs, save the file, call it bundle.js inside of the path of the
     // project directory, as a file called 'build'
-    filename: 'bundle.js' // What the filename will be when essentially combining
+    filename: 'bundle.js', // What the filename will be when essentially combining
     // all the files together
+    publicPath: 'build/'
   },
   module: {
     rules :[
@@ -17,13 +19,28 @@ output:{
         test: /\.js$/ // looks for files that end with .js, If so, babel will be applied
       },
       {
-        use:['style-loader','css-loader'],
-        //The style loader takes the CSS imports and adds them to the HTML document
-        //The css loader knows how to deal with CSS imports
+        loader: ExtractTextPlugin({
+          loader: 'css-loader'
+        }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/, // Basically ,if it's an image
+        use: [
+        {
+          loader: 'url-loader',
+          options: { limit: 40000 } // If the image is greater than 40,000, don't save it into bundle.js
+          // Save it into a seperate file
+        }, // This is used second
+        'image-webpack-loader' // This is used first, compresses the image.
+        ]
       }
      ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css') // Grab all the files that were caught by the loader
+    // and save it to a file style.css
+  ]
 };
 
 module.exports = config;
